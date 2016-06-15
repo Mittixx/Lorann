0f69a56e799c4df8dd5fbb2a10b6f70d1398015b
RomainJunca <romain.junca@viacesi.fr>
2016-06-15 11:45:21 +0200

    JavaDoc DBProperties + DAOGetMap

---
+++ model/src/main/java/model/dataBase/DAOGetMap.java
@@ -0,0 +1,23 @@
+package model.dataBase;
+
+import model.LorannWorld.Map;
+
+import java.sql.Connection;
+
+/**
+ * @author Romain
+ */
+public class DAOGetMap {
+
+    /**
+     *
+     * @param connection
+     */
+    public DAOGetMap(Connection connection) {
+    }
+
+    public Map find(int id){
+
+        return null;
+    }
+}
