7a41fa0617573be3c00f15ad5b12852a875d3005
RomainJunca <romain.junca@viacesi.fr>
2016-06-15 10:52:10 +0200

    JavaDoc Clock

---
+++ controller/src/main/java/controller/Clock.java
@@ -0,0 +1,28 @@
+package controller;
+
+/**
+ * The Class Clock
+ * @author Romain
+ */
+
+public class Clock {
+    /** The Controller */
+
+    private Controller controller;
+
+
+    /**
+     * Instanciate a new Controller
+     * @param controller
+     */
+    public Clock(Controller controller) {
+        this.controller = controller;
+    }
+
+    /**
+     * Launch a Thread.
+     */
+    public void run(){
+
+    }
+}
